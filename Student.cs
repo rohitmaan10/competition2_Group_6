8a7e7c5010e553aba349deb8e0232bbe7f492738
DamineniS <damineni@usf.edu>
2025-03-25 17:16:26 -0400

    Pushing staff and student classes

---
+++ Student.cs
@@ -0,0 +1,10 @@
+class Student : Person {
+    public string Major { get; set; }
+    public int GraduationYear { get; set; }
+    public Student(string name, string email, string id, string major, int graduationYear) 
+        : base(name, email, id)
+    {
+        Major = major;
+        GraduationYear = graduationYear;
+    }
+}
