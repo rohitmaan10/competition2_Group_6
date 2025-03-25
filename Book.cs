b8e01d52c651e3025c1bf5985ac7be57797dd678
srisatya <arumilli1@usf.edu>
2025-03-25 16:50:50 -0400

    Added Book Class r

---
+++ Book.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+// Book class
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public int AvailableCopies { get; set; }
+    public Book(string title, string author, string isbn, int availableCopies)
+    {
+        Title = title;
+        Author = author;
+        ISBN = isbn;
+        AvailableCopies = availableCopies;
+    }
+    public bool BorrowBook()
+    {
+        if (AvailableCopies > 0)
+        {
+            AvailableCopies--;
+            return true;
+        }
+        return false;
+    }
+}
+
