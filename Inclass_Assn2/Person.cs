111ef7d22c602b567bafd7013aa7fbc9df0f5bb8
DamineniS <damineni@usf.edu>
2025-03-25 16:20:33 -0400

    Pushed Person Class

---
+++ Inclass_Assn2/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+// Base class Person
+class Person {
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string ID { get; set; }
+    public Person(string name, string email, string id) {
+        Name = name;
+        Email = email;
+        ID = id;
+    }
+}
